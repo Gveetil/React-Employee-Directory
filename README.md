76a89f74b42e14bd196fe071e1644da2f64702c2
Geethanjali Veetil <gtveetil@gmail.com>
2020-06-18 14:01:11 +1000

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-Employee-Directory
-View a  centralized searchable employee directory, browse and locate employee details.
---
+++ precache-manifest.7b2425e374affcb04963924472286b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9704c0b4056520e67af2d3bd103d97fa",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "e4f66a0d97ebad55bd06",
+    "url": "/React-Employee-Directory/static/css/main.3b45058a.chunk.css"
+  },
+  {
+    "revision": "5745d9bf5fd2d98ee870",
+    "url": "/React-Employee-Directory/static/js/2.ba66b42f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Employee-Directory/static/js/2.ba66b42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f66a0d97ebad55bd06",
+    "url": "/React-Employee-Directory/static/js/main.5b4a4cfa.chunk.js"
+  },
+  {
+    "revision": "9bd509428cee10405d35",
+    "url": "/React-Employee-Directory/static/js/runtime-main.9e2f96fa.js"
+  }
+]);
