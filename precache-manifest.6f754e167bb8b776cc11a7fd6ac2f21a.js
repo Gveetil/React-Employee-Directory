1d7eb1ac658624d63d057fc7772e78f7798a90cf
Geethanjali Veetil <gtveetil@gmail.com>
2020-06-18 22:47:58 +1000

    Updates

---
+++ precache-manifest.6f754e167bb8b776cc11a7fd6ac2f21a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5978c565eb885dcc7b1fd9bb3ee043",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "30827c5f76db950612eb",
+    "url": "/React-Employee-Directory/static/css/main.3b45058a.chunk.css"
+  },
+  {
+    "revision": "7fc37e2d151d5c1f89d9",
+    "url": "/React-Employee-Directory/static/js/2.f95c7a1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Employee-Directory/static/js/2.f95c7a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30827c5f76db950612eb",
+    "url": "/React-Employee-Directory/static/js/main.4c24f3d2.chunk.js"
+  },
+  {
+    "revision": "9bd509428cee10405d35",
+    "url": "/React-Employee-Directory/static/js/runtime-main.9e2f96fa.js"
+  }
+]);
